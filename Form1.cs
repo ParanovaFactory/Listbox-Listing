0d1e64dace8bfff28cf7aef4eeaaca0b47ac3dd1
Berkay <bkaraduman64@gmail.com>
2020-12-27 20:28:54 +0300

    Listele

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayi = { 2, 85, 7, 36, 45, 86, 45, 72, 7, 6, 82, 75, 85 };
+            for(int i = 0; i < sayi.Length; i++)
+            {
+                if (sayi[i] % 2 == 0 && sayi[i] > 10)
+                {
+                    listBox1.Items.Add(sayi[i]);
+
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Listbox-Listeleme
+# Listbox Listeleme
+# if şartına uyan sisteme tanımlanmış verileri listbox'da listeleyen uygulamam
